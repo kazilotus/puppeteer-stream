44ad9e4e6bf5b8e922d01a1c65bef309c90a7c1b
kazilotus <kazilotus@hotmail.com>
2022-02-05 23:40:02 +0300

    no stealth

--- src/PuppeteerStream.ts
+++ src/PuppeteerStream.ts
@@ -10,8 +10,8 @@ import puppeteer from "puppeteer-extra";
 import { Readable, ReadableOptions } from "stream";
 import path from "path";
 
-import StealthPlugin from "puppeteer-extra-plugin-stealth";
-puppeteer.use(StealthPlugin());
+// import StealthPlugin from "puppeteer-extra-plugin-stealth";
+// puppeteer.use(StealthPlugin());
 export class Stream extends Readable {
 	constructor(private page: Page, options?: ReadableOptions) {
 		super(options);
